eadc70e8e0cfdc08ce1dd5d0a007e1f9a0938cb5
Alexey Glushchenko <alex-gl@mail.ru>
2019-07-13 16:52:20 +0900

    Traceback logging error was fixed.

--- src/pipeline/pipelinetreenode.py
+++ src/pipeline/pipelinetreenode.py
@@ -66,9 +66,10 @@ class PipelineTreeNode2:
         return result_list[0] if len(result_list) > 0 else ""
 
     @staticmethod
-    def log_error(message: str, node, exception_obj: Exception):
+    def log_error(message: str, node, exception_obj: Exception, traceback_str: str=""):
         node._logger.critical(f"{node._component}(cfg={node._cfg_count+1}, run={node._run_count}):\n"
             f"{node._get_exception_name(exception_obj)}: {message}\n"
+            f"{traceback_str}\n"
             f"Environment:\n{node._environment}\n"
             f"Parameters:\n{node._parameters}")
 
